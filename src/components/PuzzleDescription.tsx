c3325ed124d029888f281e9f96d99557c9c1c371
firesquid6 <jdeiss06@gmail.com>
2025-07-31 14:57:09 -0500

    add the next js toploader to show a loading state since we server render

--- src/components/PuzzleDescription.tsx
+++ src/components/PuzzleDescription.tsx
@@ -1,10 +1,3 @@
-import path from "path";
-
-
-function getMarkdownPath(language: string, id: string) {
-  const root = path.resolve(__dirname, "../..");
-  return path.join(root, `content/${language}/${id}.mdx`);
-}
 
 
 export default async function PuzzleDescription({ language, id }: { language: string, id: string }) {
@@ -12,7 +5,7 @@ export default async function PuzzleDescription({ language, id }: { language: st
   const Component = mod.default;
 
   return (
-    <article>
+    <article className="prose">
       <Component />
     </article>
   )
