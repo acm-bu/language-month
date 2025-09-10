fe66169cbc0b5f12903b8523b2429f0b8cb90ff9
firesquid6 <jdeiss06@gmail.com>
2025-09-09 22:45:00 -0500

    add the first lesson

--- src/server/languages/data.ts
+++ src/server/languages/data.ts
@@ -12,14 +12,20 @@ import { LanguageCourse } from ".";
 const typescript: LanguageCourse = {
   title: "Typescript",
   language: "typescript",
-  dateOpen: new Date("2025-09-01"),
+  dateOpen: new Date("2025-09-29"),
   description: "Learn typescript for web appications",
   puzzles: [
     {
       id: "day-1-hello-world",
-      date: new Date("2025-09-01"),
+      date: new Date("2025-09-29"),
       title: "Day One: Hello, Typescript!"
     },
+    {
+      id: "day-2-control-flow",
+      date: new Date("2025-09-30"),
+      title: "Day One: Hello, Typescript!"
+    },
+
   ],
 };
 
